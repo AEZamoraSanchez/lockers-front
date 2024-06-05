0870c872a9e76e21f78db946ccef89e8093d1dcb
Kyrua <ang02aezs@gmail.com>
2024-06-05 01:06:47 -0600

    build

--- src/stores/userStore/user.actions.ts
+++ src/stores/userStore/user.actions.ts
@@ -1,9 +1,10 @@
-import { createAction, props } from "@ngrx/store";
 import { userMain } from "../../utils/interfaces/userInterfaces/userMain.interface";
+import { createAction, props } from "@ngrx/store";
 
 export const updateUserMain = createAction(
   '[Home Component] Update User Main',
   props<{ user : userMain }>()
+
 )
 
 export const verReducer = createAction(
