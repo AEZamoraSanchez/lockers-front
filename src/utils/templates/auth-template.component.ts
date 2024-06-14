7e17faea7265a5af7f1f5cb606692a22543e7d86
Kyrua <ang02aezs@gmail.com>
2024-06-13 20:15:57 -0600

    image fixed

--- src/utils/templates/auth-template.component.ts
+++ src/utils/templates/auth-template.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-auth-template',
   standalone: true,
   imports: [],
-  template: `<section class="bg-[url('https://img.asmedia.epimg.net/resizer/f4HDtSPjEasJj8B_cXPKpra1CC4=/360x203/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/HCIDPA5EIZBOZMVBB6UA4JNWDU.jpg')] bg-contain w-full h-[100vh]  lg:bg-right-top login-content">
+  template: `<section class="bg-[url('https://img.asmedia.epimg.net/resizer/f4HDtSPjEasJj8B_cXPKpra1CC4=/360x203/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/HCIDPA5EIZBOZMVBB6UA4JNWDU.jpg')] bg-contain w-full h-full  lg:bg-right-top login-content">
   <section class="absolute bottom-0 left-0 w-full bg-blue-700 min-h-[75%] rounded-t-[25px] px-[20px] py-[30px] flex flex-col items-center gap-[1.9vh] justify-items-center lg:justify-center sm:px-[35px] lg:h-full lg:w-[40%] lg:rounded-t-[0] lg:min-w-[512px]">
     <ng-content></ng-content>
   </section>
