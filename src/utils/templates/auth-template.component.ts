d720a800894196357048af9cfc05fb1a21566c9c
Kyrua <ang02aezs@gmail.com>
2024-08-19 17:55:23 -0600

    login height fixed

--- src/utils/templates/auth-template.component.ts
+++ src/utils/templates/auth-template.component.ts
@@ -4,10 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-auth-template',
   standalone: true,
   imports: [],
-  template: `<section class="bg-[url('https://img.asmedia.epimg.net/resizer/f4HDtSPjEasJj8B_cXPKpra1CC4=/360x203/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/HCIDPA5EIZBOZMVBB6UA4JNWDU.jpg')] bg-contain w-full h-full  lg:bg-right-top login-content">
-  <section class="absolute bottom-0 left-0 w-full bg-blue-700 h-[75%] rounded-t-[25px] px-[20px] py-[30px] gap-[1.9vh] sm:px-[35px] lg:h-full lg:w-[40%] lg:rounded-t-[0] lg:min-w-[512px]">
+  template: `
+  <section class="bg-[url('https://img.asmedia.epimg.net/resizer/f4HDtSPjEasJj8B_cXPKpra1CC4=/360x203/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/HCIDPA5EIZBOZMVBB6UA4JNWDU.jpg')] bg-contain w-full h-[100dvh]  lg:bg-right-top login-content">
+
+  <section class="absolute bottom-0 left-0 w-full bg-blue-700 h-[75%] rounded-t-[25px] px-[20px] py-[30px] gap-[1.9vh] sm:px-[35px] lg:h-[100%] lg:w-[40%] lg:rounded-t-[0] lg:min-w-[512px]">
     <ng-content></ng-content>
   </section>
+
 </section>`,
 })
 export class AuthTemplateComponent {
